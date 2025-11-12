5a2f95828ff6bb9f1ed5712302bde2d44e8d6048
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-11-12 14:17:29 +0000

    Update embedding.py

--- FaceDetection/embedding.py
+++ FaceDetection/embedding.py
@@ -5,8 +5,8 @@ import cv2 as cv
 def embedding_photos(photo: np.ndarray):
     photo_rgb = cv.cvtColor(photo, cv.COLOR_BGR2RGB)  # Converte BGR para RGB para DeepFace processar corretamente
 
-    # Gera o embedding da foto usando o modelo Facenet
-    result = DeepFace.represent(photo_rgb, model_name="Facenet", enforce_detection=False)
+    # Gera o embedding da foto usando o modelo ArcFace
+    result = DeepFace.represent(photo_rgb, model_name="ArcFace", enforce_detection=False)
 
     embedding = np.array(result[0]["embedding"], dtype=np.float32)
 
